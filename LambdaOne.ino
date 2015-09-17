75fd809bdcca2254ff539bf971d59d9e1e11c0f9
DiamondValve <diamondvalve@protonmail.com>
2015-09-18 05:07:41 +0800

    v9.0
    fixed a few things and changed naming scheme.
    changed the defines to const int because of how often they are used a
    well

--- LambdaOne.ino
+++ LambdaOne.ino
@@ -1,173 +1,169 @@
 
 /*
  :::      :::====  :::=======  :::====  :::====  :::====  :::=====
- :::      :::  === ::: === === :::  === :::  === :::  === :::     
- ===      ======== === === === =======  ===  === ======== ===     
- ===      ===  === ===     === ===  === ===  === ===  === ===     
+ :::      :::  === ::: === === :::  === :::  === :::  === :::
+ ===      ======== === === === =======  ===  === ======== ===
+ ===      ===  === ===     === ===  === ===  === ===  === ===
  ======== ===  === ===     === =======  =======  ===  ===  =======
-    v0.8 - Release Candidate
+    v0.9 - Release Candidate
     Created by Josh Perry and Collin Rodes
     Code licenced under the "MIT" license.
 */
 #include "MotorDriver.h"
 #include <Servo.h>
-#define  TRIGGER_PIN       13
-#define  ECHO_PIN          12
-#define  TRIGGER_PINLEFT   9
-#define  ECHO_PINLEFT      8
-#define  TRIGGER_PINRIGHT  11
-#define  ECHO_PINRIGHT     10
-#define  TRIGGER_PINBACK   7
-#define  ECHO_PINBACK      6
-
-Servo pivot;
-Servo pivot2;
-
-long LineFront = analogRead(A3);
-long LineBack = analogRead(A2);
-long duration, distance, durationleft, distanceleft, durationright, distanceright, durationback, distanceback;
-// int brake = 5;
-// or we could just use motordriver.goBackwards() with an accelorometer
-int straight = 90;
-int left = 45;
-int right = 135;
+const int  frontTriggerPin = 13;
+const int  frontEchoPin = 12;
+const int  leftTriggerPin = 9;
+const int  leftEchoPin = 8;
+const int  rightTriggerPin = 11;
+const int  rightEchoPin = 10;
+const int  backTriggerPin = 7;
+const int  backEchoPin = 6;
+const int straight = 90;
+const int left = 45;
+const int right = 135;
+// const int brake;
 int white;
 int black;
 int maxdistance;
-void ultrasonic();
-void nooneshome();
-void control();
+long frontLine = analogRead(A3);
+long backLine = analogRead(A2);
+long durationFront, distanceFront, durationLeft, distanceLeft, durationRight, distanceRight, durationBack, distanceBack;
+void spinSearch();
+void ultrasonicPing();
+void bestOption();
+Servo pivotFront;
+Servo pivotBack;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(TRIGGER_PIN, OUTPUT);
-  pinMode(ECHO_PIN, INPUT);
-  pinMode(TRIGGER_PINLEFT, OUTPUT);
-  pinMode(ECHO_PINLEFT, INPUT);
-  pinMode(TRIGGER_PINRIGHT, OUTPUT);
-  pinMode(ECHO_PINRIGHT, INPUT);
-  pinMode(TRIGGER_PINBACK, OUTPUT);
-  pinMode(ECHO_PINBACK, INPUT);
-  pivot2.attach(3);
-  pivot.attach(2);
+  pinMode(frontTriggerPin, OUTPUT);
+  pinMode(frontEchoPin, INPUT);
+  pinMode(leftTriggerPin, OUTPUT);
+  pinMode(leftEchoPin, INPUT);
+  pinMode(rightTriggerPin, OUTPUT);
+  pinMode(rightEchoPin, INPUT);
+  pinMode(backTriggerPin, OUTPUT);
+  pinMode(backEchoPin, INPUT);
+  pivotBack.attach(3);
+  pivotFront.attach(2);
 }
 
-void frontfunc() {
-  while ((distance < distanceleft, distanceright, distanceback) and (maxdistance >= distance)) {
-    pivot.write(straight);
-    pivot2.write(straight);
+void frontFuctions() {
+  while ((distanceFront < distanceLeft, distanceRight, distanceBack) and (maxdistance >= distanceFront)) {
+    pivotFront.write(straight);
+    pivotBack.write(straight);
     motordriver.goForward();
-    control();
+    bestOption();
   }
 }
 
-void leftfunc() {
-  while ((distanceleft < distanceright, distanceback, distance) and (maxdistance >= distanceleft)) {
-    pivot.write(left);
-    pivot2.write(left);
+void leftFunctions() {
+  while ((distanceLeft < distanceRight, distanceBack, distanceFront) and (maxdistance >= distanceLeft)) {
+    pivotFront.write(left);
+    pivotBack.write(left);
     motordriver.goForward();
-    control();
+    bestOption();
   }
 }
 
-void rightfunc() {
-  while ((distanceright < distanceback, distance, distanceleft) and (maxdistance >= distanceright)) {
-    pivot.write(right);
-    pivot2.write(right);
+void rightFunctions() {
+  while ((distanceRight < distanceBack, distanceFront, distanceLeft) and (maxdistance >= distanceRight)) {
+    pivotFront.write(right);
+    pivotBack.write(right);
     motordriver.goForward();
-    control();
+    bestOption();
   }
 }
 
-void backfunc() {
-  while ((distanceback < distance, distanceleft, distanceright) and (maxdistance >= distanceback)) {
+void backFuctions() {
+  while ((distanceBack < distanceFront, distanceLeft, distanceRight) and (maxdistance >= distanceBack)) {
     motordriver.goBackward();
   }
-  control();
+  bestOption();
 }
 
-
-
-
-void nooneshome() {
-  if (distanceleft > distanceright) {
-    pivot.write(left);
-    pivot2.write(straight);
+void spinSearch() {
+  if ((distanceLeft > distanceRight) and ((frontLine, backLine) = black)) {
+    pivotFront.write(left);
+    pivotBack.write(straight);
     motordriver.goForward();
     delay(2000);
     motordriver.stop();
   }
-  if (distanceright > distanceleft) {
-    pivot.write(right);
-    pivot2.write(straight);
+  if ((distanceRight > distanceLeft) and ((frontLine, backLine) = black)) {
+    pivotFront.write(right);
+    pivotBack.write(straight);
     motordriver.goForward();
     delay(2000);
     motordriver.stop();
   }
-  ultrasonic();
+  ultrasonicPing();
 }
 
-void control() {
-  if ((LineFront = black) and (LineBack = black)){
-  if ((distance < distanceleft, distanceright, distanceback) and (maxdistance >= distance)) {
-    frontfunc();
+void bestOption() {
+  if ((frontLine = black) and (backLine = black)) {
+    if ((distanceFront < distanceLeft, distanceRight, distanceBack) and (maxdistance >= distanceFront)) {
+      frontFuctions();
+    }
+    if ((distanceLeft < distanceRight, distanceBack, distanceFront) and (maxdistance >= distanceLeft)) {
+      leftFunctions();
+    }
+    if ((distanceRight < distanceBack, distanceFront, distanceLeft) and (maxdistance >= distanceRight)) {
+      rightFunctions();
+    }
+    if ((distanceBack < distanceFront, distanceLeft, distanceRight) and (maxdistance >= distanceBack)) {
+      backFuctions();
+    }
   }
-  if ((distanceleft < distanceright, distanceback, distance) and (maxdistance >= distanceleft)) {
-    leftfunc();
-  }
-  if ((distanceright < distanceback, distance, distanceleft) and (maxdistance >= distanceright)) {
-    rightfunc();
-  }
-  if ((distanceback < distance, distanceleft, distanceright) and (maxdistance >= distanceback)) {
-    backfunc();
-  }
-  }
-  else{
-    while ((LineFront = black) and (LineBack = white)){
+  else {
+    while ((frontLine = black) and (backLine = white)) {
       motordriver.goForward();
     }
-    while ((LineFront = white) and (LineBack = black))
+    while ((frontLine = white) and (backLine = black)) {
       motordriver.goBackward();
+    }
+    spinSearch();
   }
 }
 
-void ultrasonic() {
-  digitalWrite(TRIGGER_PIN, LOW);
+void ultrasonicPing() {
+  digitalWrite(frontTriggerPin, LOW);
   delayMicroseconds(2);
-  digitalWrite(TRIGGER_PIN, HIGH);
+  digitalWrite(frontTriggerPin, HIGH);
   delayMicroseconds(10);
-  digitalWrite(TRIGGER_PIN, LOW);
-  duration = pulseIn(ECHO_PIN, HIGH);
-  distance = (duration / 2) / 29.1;
-  digitalWrite(TRIGGER_PINLEFT, LOW);
+  digitalWrite(frontTriggerPin, LOW);
+  durationFront = pulseIn(frontEchoPin, HIGH);
+  distanceFront = (durationFront / 2) / 29.1;
+  digitalWrite(leftTriggerPin, LOW);
   delayMicroseconds(2);
-  digitalWrite(TRIGGER_PINLEFT, HIGH);
+  digitalWrite(leftTriggerPin, HIGH);
   delayMicroseconds(10);
-  digitalWrite(TRIGGER_PINLEFT, LOW);
-  durationleft = pulseIn(ECHO_PINLEFT, HIGH);
-  distanceleft = (durationleft / 2) / 29.1;
-  digitalWrite(TRIGGER_PINRIGHT, LOW);
+  digitalWrite(leftTriggerPin, LOW);
+  durationLeft = pulseIn(leftEchoPin, HIGH);
+  distanceLeft = (durationLeft / 2) / 29.1;
+  digitalWrite(rightTriggerPin, LOW);
   delayMicroseconds(2);
-  digitalWrite(TRIGGER_PINRIGHT, HIGH);
+  digitalWrite(rightTriggerPin, HIGH);
   delayMicroseconds(10);
-  digitalWrite(TRIGGER_PINRIGHT, LOW);
-  durationright = pulseIn(ECHO_PINRIGHT, HIGH);
-  distanceright = (durationright / 2) / 29.1;
-  digitalWrite(TRIGGER_PINBACK, LOW);
+  digitalWrite(rightTriggerPin, LOW);
+  durationRight = pulseIn(rightEchoPin, HIGH);
+  distanceRight = (durationRight / 2) / 29.1;
+  digitalWrite(backTriggerPin, LOW);
   delayMicroseconds(2);
-  digitalWrite(TRIGGER_PINBACK, HIGH);
+  digitalWrite(backTriggerPin, HIGH);
   delayMicroseconds(10);
-  digitalWrite(TRIGGER_PINBACK, LOW);
-  durationback = pulseIn(ECHO_PINBACK, HIGH);
-  distanceback = (durationback / 2) / 29.1;
-  control();
+  digitalWrite(backTriggerPin, LOW);
+  durationBack = pulseIn(backEchoPin, HIGH);
+  distanceBack = (durationBack / 2) / 29.1;
+  bestOption();
 }
 
 void loop() {
-  pivot2.write(left);
-  pivot.write(right);
+  pivotBack.write(left);
+  pivotFront.write(right);
   motordriver.goForward();
   delay(2000);
   motordriver.stop();
-  ultrasonic();
+  ultrasonicPing();
 }
